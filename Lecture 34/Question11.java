e502b98d3e8cd7e7b687b283f0707c1d303b0ebc
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 17:19:25 +0530

    Update Question11.java

--- Lecture 34/Question11.java
+++ Lecture 34/Question11.java
@@ -1,1 +1,26 @@
-
+package q11275;
+public class PolymorphismExample2 {
+	public static void main(String[] args) {
+		A a = new A();
+		A b = new B();
+		C c = new C();
+		System.out.println(a.getName());
+		System.out.println(b.getName());
+		System.out.println(c.getName());
+	}
+}
+class A {
+	public String getName() {
+		return "A";
+	}
+}
+class B extends A {
+	public String getName() {
+		return super.getName() + " " + "B";
+	}
+}
+class C extends B {
+	public String getName() {
+		return super.getName() + " " + "C";
+	}
+}
