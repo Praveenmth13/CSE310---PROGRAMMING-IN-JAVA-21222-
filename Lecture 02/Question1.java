aac50f7ebb541a560c936942307c63bbff312df7
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:08:32 +0530

    Create Question1.java

---
+++ Lecture 02/Question1.java
@@ -0,0 +1,5 @@
+Answer 
+
+The Java compiler uses the Java source file and generates file with .class extension.
+The file with .class extension is called as Java class file.
+The Java class file contains the Java bytecode, which is executed by the Java Virtual Machine (JVM).
