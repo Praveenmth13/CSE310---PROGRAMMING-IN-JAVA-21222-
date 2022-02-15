ea595bc3e716b55edf84fe3b3f89d87a91d6c137
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 18:04:49 +0530

    Create Question03.java

---
+++ Lecture 35/Question03.java
@@ -0,0 +1,3 @@
+                            //Answer
+
+Statements 3 and 4 will produce compiler errors saying value2 cannot be resolved, since value2 is neither declared locally in the enclosing block nor as a field in the class.
