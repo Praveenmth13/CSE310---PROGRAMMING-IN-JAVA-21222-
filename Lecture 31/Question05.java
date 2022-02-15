f85d42e4f2b20bc9f2936cf88adaa83599a2a2a6
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:31:51 +0530

    Create Question05.java

---
+++ Lecture 31/Question05.java
@@ -0,0 +1,8 @@
+package q11195;
+public class TestSubstring {
+	public static void main(String[] args)
+	{
+		int size=(args[0].length());
+		System.out.println(args[0].substring(0,size/2));
+		}
+	}
