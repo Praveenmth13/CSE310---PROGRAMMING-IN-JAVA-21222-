e84b71c877422ea1e6127d0b6479a6eff9570c9b
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 14:00:05 +0530

    Create Question01.java

---
+++ Lecture 20/Question01.java
@@ -0,0 +1,19 @@
+package q11039;
+public class BubbleSorting{
+	public static void bubbleSort(int[] array){
+		int arraySize = array.length;
+		int temp;
+		for(int i = 0; i<arraySize-1;i++){
+			for(int j=0; j<arraySize-i-1;j++){
+			if(array[j] > array[j+1]){
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+	for(int k = 0; k<arraySize; k++){
+		System.out.println(array[k]);
+	}
+}
+}
