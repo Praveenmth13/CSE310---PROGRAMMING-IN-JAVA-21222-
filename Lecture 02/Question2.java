9479ef8ee2276840e83522950ef681c48de49278
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:09:36 +0530

    Create Question2.java

---
+++ Lecture 02/Question2.java
@@ -0,0 +1,3 @@
+
+                                                  //Answer 
+Java class libraries contain the class files of standard Java classes required for running Java Applications or Applets
