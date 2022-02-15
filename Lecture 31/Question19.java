c42792bd53857de57bf2eaf477b34525818dfe0c
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:40:04 +0530

    Create Question19.java

---
+++ Lecture 31/Question19.java
@@ -0,0 +1,7 @@
+package q11209;
+public class CheckChars{
+	public static void main(String[] args){
+		String input = args[0];
+		System.out.println(input.contains("w") && input.contains("e") && input.contains("b"));
+	}
+}
