5434b3f2d2f75eec826508d9809d50d25ea3abfa
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 17:15:24 +0530

    Update Question05.java

--- Lecture 34/Question05.java
+++ Lecture 34/Question05.java
@@ -1,1 +1,27 @@
-
+package q11269;
+public class InheritanceExample {
+	public static void main(String[] args) {
+		A a1 = new A();
+		System.out.println("a1.getAValue() : " + a1.getAValue());
+		B b = new B();
+		System.out.println("b.getBValue() : " + b.getBValue());
+		System.out.println("b.getAValue() : " + b.getAValue());
+		A a2 = b;
+		System.out.println("a2.getAValue() : " + a2.getAValue());
+	}
+}
+class A {
+	public int aValue = 2;
+	public int getAValue() {
+		return aValue;
+	}
+}
+class B extends A {
+	public int bValue = 3;
+	public int getBValue() {
+		return bValue;
+	}
+	public int getAValue() {
+		return 2 * aValue;
+	}
+}
