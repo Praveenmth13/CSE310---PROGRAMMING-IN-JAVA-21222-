f2bdd0565b1cb1fb3e3ec84e3ce5af9eaff43e6a
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:38:55 +0530

    Create Question18.java

---
+++ Lecture 31/Question18.java
@@ -0,0 +1,11 @@
+package q11208;
+public class MiddleChar{
+public static void main(String[] args){
+	String input = args[0];
+	if(input.length()%2==0){
+		System.out.println(input.charAt(input.length()/2));
+	} else {
+		System.out.println(input.charAt(input.length()/2));
+	}
+}
+}
