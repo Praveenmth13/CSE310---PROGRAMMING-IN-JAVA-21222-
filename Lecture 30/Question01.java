7c33609e60e124d3e90bce7893a3d2236e21e4ae
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:45:31 +0530

    Create Question01.java

---
+++ Lecture 30/Question01.java
@@ -0,0 +1,11 @@
+package q11185;
+public class StringFormatting {
+	public static void main(String[] args) {
+		int iVar = 432000;
+		System.out.printf("Integer variable = %d%n", iVar);
+		double dVar = 3.14159;
+		System.out.printf("Double variable = %f%n", dVar);
+		String str = "Demo";
+		System.out.printf("String variable = %s%n", str);
+	}
+}
