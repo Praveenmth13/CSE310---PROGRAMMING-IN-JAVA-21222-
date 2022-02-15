5b15f303cd36c1a505a5e985c587ab181487f89a
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 16:30:58 +0530

    Create Question04.java

---
+++ Lecture 32/Question04.java
@@ -0,0 +1,8 @@
+                                            //Answer
+
+In statement 1, there should not be a space between the method name and the opening parenthesis. isPrimeNumber (
+In statement 2, there should be a space between for and the opening parenthesis that follows it.
+In statement 2, there should be a space on either side of = (assignment operator) in the initialization expression int i=2;.
+In statement 2, there should be a space on either side of / and <= operators in the condition expression i<=(number/2);.
+In statement 2, there should be a space after the semicolon ; of the condition expression i<=(number/2);.
+In statement 3, there should be a space on either side of % and == operators in the if's condition expression (number%i)==0.
