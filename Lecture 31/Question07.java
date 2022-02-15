75329451f09a33469ebb02a284a1b54caeeb9e78
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:33:07 +0530

    Create Question07.java

---
+++ Lecture 31/Question07.java
@@ -0,0 +1,8 @@
+package q11197;
+public class StringStairs{
+	public static void main(String[] args){
+		for(int i=1;i<args[0].length()+1;i++){
+			System.out.println(args[0].substring(0,i));
+		}
+	}
+}
