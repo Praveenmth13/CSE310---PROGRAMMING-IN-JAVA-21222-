7e9eb7b0f35a8a0c58bee1b75191819b9287d123
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 17:20:48 +0530

    Update Question14.java

--- Lecture 34/Question14.java
+++ Lecture 34/Question14.java
@@ -1,1 +1,14 @@
-
+package q11278;
+public class GetClassExample {
+	public static void main(String[] args) {
+		A a = new A();
+		B b = new B();
+		String text = "Ganga";
+		System.out.println("a.getClass() : " + a.getClass());
+		System.out.println("b.getClass() : " + b.getClass());
+		System.out.println("text.getClass() : " + text.getClass());
+	}
+}
+class A {
+}
+class B {
