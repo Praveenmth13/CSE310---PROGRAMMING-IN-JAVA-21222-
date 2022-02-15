93541706bf57bf3be1673355b884b9a7029d1a2f
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 16:28:41 +0530

    Create Question02.java

---
+++ Lecture 32/Question02.java
@@ -0,0 +1,9 @@
+package q11239;
+public class MathExamples {
+	public static void main(String[] args) {
+		double base = 4;
+		double exponent = 3;
+		System.out.println(base + " to the power " + exponent + " is : " + Math.pow(base, exponent));
+		System.out.println("Square root of " + base + " is : " + Math.sqrt(base));
+	}
+}
