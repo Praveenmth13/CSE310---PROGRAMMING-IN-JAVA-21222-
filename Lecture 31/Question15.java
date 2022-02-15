d95fe162c31df46afaf00a1da978c7cfd32c0981
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 13:37:34 +0530

    Create Question15.java

---
+++ Lecture 31/Question15.java
@@ -0,0 +1,8 @@
+package q11205;
+public class EndsWith {
+	public static void main(String[] args){
+		boolean ends = args[0].toLowerCase().endsWith(args[1].toLowerCase());
+		System.out.println(ends ? ends : ends);
+	}
+	
+}
