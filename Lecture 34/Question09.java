8a4abe64000e60377a85fc28291a7509b3920fca
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 17:18:15 +0530

    Update Question09.java

--- Lecture 34/Question09.java
+++ Lecture 34/Question09.java
@@ -1,1 +1,38 @@
-
+package q11273;
+class Animal {
+	public Animal() {
+		System.out.println("Animal is created");
+		
+	}
+	void eat() {
+		System.out.println("Eating something");
+		
+	}  
+}  
+class Dog extends Animal { 
+	public Dog() {
+		System.out.println("Dog is created");
+		
+		
+	}
+	void eat() {
+		System.out.println("Eating bread");
+		
+	}
+	void bark() {
+		System.out.println("Barking");
+		
+	}  
+	void work() {  
+		super.eat();
+		eat();
+		bark();
+		
+	}  
+}  
+public class ExampleOnSuper { 
+	public static void main(String args[]) {
+		Dog d = new Dog();  
+		d.work();  
+	}
+}
