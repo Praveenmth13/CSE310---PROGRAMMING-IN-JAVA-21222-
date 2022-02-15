47b4f0d059279f408bdca60bc462f35f1a0c166a
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-15 16:47:58 +0530

    Create Question03.java

---
+++ Lecture 37/Question03.java
@@ -0,0 +1,65 @@
+package q11287;
+
+//Write the code
+
+public class AbstractExample {
+	public static void main(String[] args) {
+		Shape s;
+		s = new Trapezoid();
+		s.numberofsides();
+		s = new Triangle();
+		s.numberofsides();
+		s = new Hexagon();
+		s.numberofsides();
+	}
+}
+
+abstract class Shape
+
+{
+	
+	abstract void numberofsides();
+	
+}
+
+class Trapezoid extends Shape
+
+{
+	
+	void numberofsides()
+	
+	{
+		
+		System.out.println("Number of sides in a trapezoid are 4");
+		
+	}
+	
+}
+
+class Triangle extends Shape
+
+{
+	
+	void numberofsides()
+	
+	{
+		
+		System.out.println("Number of sides in a triangle are 3");
+		
+	}
+	
+}
+
+class Hexagon extends Shape
+
+{
+	
+	void numberofsides()
+	
+	{
+		
+		System.out.println("Number of sides in a hexagon are 6");
+		
+	}
+	
+}
