a26a55926f51268d145c83030ece3d59609fd957
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 21:53:01 +0530

    Update Question01.java

--- Lecture 41/Question01.java
+++ Lecture 41/Question01.java
@@ -1,1 +1,29 @@
-
+package q11296;
+public class A {
+	private int value = 7;
+	class B {
+		private B() {
+			System.out.println("In inner class B's constructor");
+		}
+	}
+	static class C {
+		private C() {
+			System.out.println("In static nested class C's constructor");
+		}
+	}
+	public void someMethodInClassA() {
+		class D {
+			private D() {
+				System.out.println("In local class D's constructor");
+				System.out.println("value = " + value);
+			}
+		}
+		D d = new D();
+	}
+	public static void main(String[] args) {
+		A a = new A();
+		A.B b = new A().new B();
+		A.C c = new A.C();
+		a.someMethodInClassA();
+	}
+}
