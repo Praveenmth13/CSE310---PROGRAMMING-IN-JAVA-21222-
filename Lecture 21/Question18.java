907071c89ce8f809b379c469f280541b52ad18b7
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 22:15:51 +0530

    Create Question18.java

---
+++ Lecture 21/Question18.java
@@ -0,0 +1,21 @@
+package q11066;
+
+public class CompareArrays {
+	/**
+	 * Find the length of the two arrays are equal or not
+	 * 
+	 * 
+	 * @return result
+	 */ 
+	
+	
+	
+	public boolean compareArrays(int[] arr1, int[] arr2) {
+		//Write your code here
+		if(arr1.length == arr2.length)
+		{
+			return true;
+		}
+		return false;
+	}
+}
