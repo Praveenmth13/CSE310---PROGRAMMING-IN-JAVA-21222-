760a801fe3337b147fb408d9e823b32fbeaa8273
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 22:13:26 +0530

    Create Question15.java

---
+++ Lecture 21/Question15.java
@@ -0,0 +1,21 @@
+package q11063;
+
+public class InitializeArray {
+	/**
+	 * Set all the elemets in the array to given element and set to length of array to given length
+	 * 
+	 * 
+	 * 
+	 * @return arry 
+	 */
+	 
+	public int[] initialize(int len, int ele) {
+		//Write  your code here
+		int arr[] = new int[len];
+		for(int i = 0; i<len; i++)
+		{
+			arr[i]=ele;
+		}
+		return arr;
+	}
+}
