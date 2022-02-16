e16be3299c7fe48562f56210e469df31e842488a
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 22:17:22 +0530

    Update Question20.java

--- Lecture 21/Question20.java
+++ Lecture 21/Question20.java
@@ -1,1 +1,28 @@
-
+package q11068;
+public class ElementCheck {
+	/**
+	 * Find if the arr contains only arg1 or arg2 elements
+	 * 
+	 * 
+	 * @return result
+	 */ 
+	
+	public boolean checkElement(int[] arr, int arg1, int arg2) {
+		
+		//Write your code here
+		int count=0;
+		for(int i=0; i<arr.length;i++)
+		{
+			if(arr[i] == arg1 || arr[i] == arg2)
+			{
+				count++;
+			}
+		}
+		if(count == arr.length)
+		{
+			return true;
+		}
+		else
+		return false;
+	}
+}
