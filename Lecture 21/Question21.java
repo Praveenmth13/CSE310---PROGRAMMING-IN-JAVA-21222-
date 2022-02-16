92c9b5cfe301f7a5d2ef4d49bec684802a098af9
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 22:18:34 +0530

    Create Question21.java

---
+++ Lecture 21/Question21.java
@@ -0,0 +1,27 @@
+package q11069;
+
+
+public class CountEvens {
+	/**
+	 * Find the count of even numbers in the given array
+	 * 
+	 * 
+	 * 
+	 * @return count
+	 * 
+	 */ 
+	
+	
+	public int countEvens(int[] arr) {
+		
+		//Write your code here
+		int count = 0;
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]%2 == 0){
+				count++;
+			}
+		}
+		return count;
+	}
+		
+	}
