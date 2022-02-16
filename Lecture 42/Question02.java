0a3f1aa2fceaeb4737797db39bc9dd81769c25c2
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 22:29:19 +0530

    Create Question02.java

---
+++ Lecture 42/Question02.java
@@ -0,0 +1,5 @@
+
+                  ANSWER
+
+C.) Statement 4 will not result in a compilation error, since field comments is not declared as final.
+D.) Statement 6 will result in a compilation error.
