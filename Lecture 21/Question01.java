3c329c07d8da9c330b69cbec45572a9f81cd5daf
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-16 22:01:03 +0530

    Update Question01.java

--- Lecture 21/Question01.java
+++ Lecture 21/Question01.java
@@ -1,1 +1,11 @@
-
+package q11046;
+public class SumOfElements {
+	public long sum(int[] arr) {
+		long sum=0;
+		// Write the code
+		for(int i=0 ; i<arr.length ; i++){
+			sum=sum+arr[i];
+		}
+		return sum;	
+	}
+}
