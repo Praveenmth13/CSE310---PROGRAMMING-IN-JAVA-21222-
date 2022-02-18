4825b01b7ec824ecf16044e7e30cfd6cc65b89ae
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-18 08:50:17 +0530

    Create Question11.java

---
+++ Lecture 44/Question11.java
@@ -0,0 +1,10 @@
+Given
+public class StringBuilderDemo {
+	public static void main(String args[]) {
+        String s = "Hello";
+        s.concat("World");
+        System.out.println(s);  
+	}
+}
+What will be the output for the above program. Choose the correct option form the below.
+B.) Hello    //ANSWER
