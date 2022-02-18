55002b0e42a1adc03792ba9b6e16cd5ed6d0c8d4
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-18 09:14:38 +0530

    Create Question03.java

---
+++ Lecture 45/Question03.java
@@ -0,0 +1,16 @@
+Select all the correct statements for the below code:
+public class Demo {
+	public static void main(String[] args) {
+		System.out.print("Up ");
+		System.out.print("up");
+		System.out.print(" and away!");
+	}
+}
+A.) There is no print method there is only println, so the above code will not compile.
+B.) The print method used is in System class.
+C.)The output will be as given below:
+  Up 
+  up 
+  and away!
+D.)The output will be as given below:
+Up up and away!  //Answer
