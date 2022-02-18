68bc970e048c2656f1089e28e8deaf3642ea43f5
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-18 09:15:15 +0530

    Create Question04.java

---
+++ Lecture 45/Question04.java
@@ -0,0 +1,12 @@
+package q11312;
+public class TimingExample {
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		int total = 0;
+		for (int i = 0; i < 10000; i++) {
+			total = total + i;
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Time taken in milliseconds = " + (endTime - startTime));
+	}
+}
