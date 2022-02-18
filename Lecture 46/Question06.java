722f21dc78b8c771130b5f0ac08c927b64040fcd
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-18 09:24:55 +0530

    Create Question06.java

---
+++ Lecture 46/Question06.java
@@ -0,0 +1,13 @@
+Given :
+import java.util.Date;
+import java.text.DateFormat;
+
+DateFormat df;
+Date date = new Date();
+// insert code here
+String s = df.format(date);
+Which code fragment, inserted at // insert code here, allows the code to compile?
+
+[Hint: Click on Date and DateFormat to explore the methods.]
+
+D.) df = DateFormat.getInstance(); //ANSWER
