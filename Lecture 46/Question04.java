ae7a23fa49ac8ab071acab29989c647b87b0d233
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-18 09:17:56 +0530

    Create Question04.java

---
+++ Lecture 46/Question04.java
@@ -0,0 +1,14 @@
+package q11397;
+import java.util.*;
+public class DateExample {
+	public static void main(String ... args) throws InterruptedException {
+		System.out.println("Sleeping for 1 second...");
+		Date timeAsDate = new Date();
+		long oldTimeInMillis = timeAsDate.getTime();
+		Thread.sleep(1000);
+		long newCurrentTimeInMillis = System.currentTimeMillis();
+		if (newCurrentTimeInMillis - oldTimeInMillis >= 1000) {
+			System.out.println("This thread resumed after 1 or more seconds");
+		}
+	}
+}
