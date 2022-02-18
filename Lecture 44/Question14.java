ca643661f2519a9c2988d686d4bbf51cc7346960
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-18 08:55:45 +0530

    Create Question14.java

---
+++ Lecture 44/Question14.java
@@ -0,0 +1,5 @@
+
+                    //ANSWER
+A.) Initial capacity of string buffer constructor is 16 characters.
+
+B.) public StringBuffer(CharSequence seq), if seq is null it throws NullPointerExceotion.
