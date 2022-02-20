3d121fa3fa7b6c01c65ae545441d71f63e254b51
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-21 00:21:28 +0530

    Create Question07.java

---
+++ Lecture 48/Question07.java
@@ -0,0 +1,43 @@
+package q11331;
+public class MultiCatchBlocks {
+	// Write the code
+	public void multiCatch(int[] arr, int index){
+		
+		try{
+			
+			System.out.println(arr[index]);
+			
+			System.out.println(arr[index]/index);
+			
+		
+			
+		}
+		
+		catch(ArithmeticException e)
+		
+		{
+			
+			System.out.println("Division by zero exception occurred");
+			
+		}
+		
+		catch(ArrayIndexOutOfBoundsException e)
+		
+		{
+			
+			System.out.println("Array index out of bounds exception occurred");
+			
+		}
+		
+		catch(Exception e)
+		
+		{
+			
+			System.out.println("Exception occurred");
+			
+		}
+		
+	}
+	
+	
+}
