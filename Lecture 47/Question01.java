4975ee21dcd2f88265c7f4e6d664aa99ff5403bc
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-21 00:07:02 +0530

    Update Question01.java

--- Lecture 47/Question01.java
+++ Lecture 47/Question01.java
@@ -1,1 +1,9 @@
-
+package q11317;
+public class ExceptionDemo1 {
+	public static void main(String[] args) {
+		int number = 34;
+		int divisor = 2;
+		int result = number / divisor;
+		System.out.println("result = " + result);
+	}
+}
