5f60bb1e6a143f2305878360899d2860c610fb3b
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-21 00:12:04 +0530

    Create Question07.java

---
+++ Lecture 47/Question07.java
@@ -0,0 +1,16 @@
+package q11323;
+public class TryCatchDemo2 {
+	public static void main(String[] args) {
+		String text1 = "33";
+		String text2 = "44a";
+		System.out.println("before parsing text1");
+		int value1 = Integer.parseInt(text1);
+		System.out.println("value1 = " + value1);
+		try {
+			int value2 = Integer.parseInt(text2);
+			System.out.println("value2 = " + value2);
+		} catch (NumberFormatException e) {
+			System.out.println("could not parse text2 = " + text2);
+		}
+	}
+}
