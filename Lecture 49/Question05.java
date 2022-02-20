7508a4b0d333f356927009ce6e56c1b251551c5c
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-21 00:32:53 +0530

    Create Question05.java

---
+++ Lecture 49/Question05.java
@@ -0,0 +1,21 @@
+package q11338;
+public class MyException {
+	public static void main (String[] args) {
+		int num=Integer.parseInt(args[0]);
+		try {
+			int x = Integer.parseInt(args[0]);
+			if (num>=25 && num<=50) // write the condition
+	
+				System.out.println("Given number : " + x);
+				else throw new NumberRangeException();
+		}
+		catch (NumberRangeException e) {
+			System.out.println("q11338.NumberRangeException"); // Fill the missing code
+		}
+	}
+}
+class NumberRangeException extends Exception {
+public	NumberRangeException(){
+		System.out.println("Please enter a number between 25 and 50");
+	} 
+}
