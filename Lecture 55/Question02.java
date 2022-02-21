d0aec7a22dac1ed4f4c90fe7e12c1aa265bf9d0f
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-21 22:12:51 +0530

    Create Question02.java

---
+++ Lecture 55/Question02.java
@@ -0,0 +1,5 @@
+      Answers
+
+A.) In annotations @ indicates to compiler that what an annotation does.
+B.) Override is a predefined java annotation.
+C.) We can use multiple annotations in one declaration
