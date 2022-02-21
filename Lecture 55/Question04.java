9fda6a3e12acf723121df07caf039f4807f9695d
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-21 22:15:28 +0530

    Create Question04.java

---
+++ Lecture 55/Question04.java
@@ -0,0 +1,5 @@
+    Answers
+
+A.) Annotations that are applied to another annotations are called meta annotations.
+C.) @Retention specifies that how a marked annotations are stored
+D.) ElementType.METHOD can be applied to a method-level annotation
