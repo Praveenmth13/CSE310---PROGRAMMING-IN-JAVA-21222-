1d682361ce9e6e64dcb8d873aa0672c1f9bcb946
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-22 22:16:45 +0530

    Update Question01.java

--- Lecture 61/Question01.java
+++ Lecture 61/Question01.java
@@ -1,1 +1,15 @@
-
+package q11388;
+import java.util.*;
+public class GenericListDemo {
+	public static void main(String[] args) {
+		List<Integer> numbersList = new ArrayList<Integer>();
+		numbersList.add(new Integer(72));
+		numbersList.add(78);
+		numbersList.add(81);
+		int total = 0;
+		for (int number : numbersList) {
+			total = total + number;
+		}
+		System.out.println("total = " + total);
+	}
+}
