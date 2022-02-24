1c18112993a00e8e991a66dabcfe1dbcec6737c8
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-24 18:38:20 +0530

    Create Question07.java

---
+++ Lecture 57/Question07.java
@@ -0,0 +1,20 @@
+package q11958;
+import java.util.*;
+public class ListDemo {
+	public static void main(String[] args) {
+		List<String> namesList = new ArrayList<String>();
+		namesList.add("Mercury");
+		namesList.add("Venus");
+		namesList.add("Earth");
+		namesList.add("Mars");
+		System.out.println(namesList.get(1)); // write your logic here
+	}
+}
+
+
+
+
+
+
+
+
