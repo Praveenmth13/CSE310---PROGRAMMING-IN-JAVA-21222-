2f663dcfa2f3b6d2cfd7180ce1702a66892cd480
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-24 17:43:33 +0530

    Create Question03.java

---
+++ Lecture 56/Question03.java
@@ -0,0 +1,4 @@
+=> ANSWER
+The size() method in the collection interface returns an int value which contains the count of elements present in that collection.
+The clear() method in the collection interface removes all the elements present in that collection.
+The isEmpty() method in the collection interface returns true, if the collection is empty and returns false otherwise.
