4dba28d8b621b88a23017a0b1fa1ebe415ac0275
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-24 20:49:01 +0530

    Create Question10.java

---
+++ Lecture 57/Question10.java
@@ -0,0 +1,23 @@
+package q24078;
+import java.util.*;
+public class ListDemo {
+	public static void main(String[] args) {
+		// Notice the use of generics. We will learn more about them later.
+		// The type parameter <String> will ensure that your code cannot add any 
+		// other object than those of type String
+		List<String> namesList = new ArrayList<String>();
+		// write your code below this
+		namesList.add ("Sunday");
+		namesList.add ("Monday");
+		namesList.add ("Tuesday");
+		namesList.add ("Wednesday");
+		namesList.add ("Thursday");
+		namesList.add ("Friday");
+		namesList.add ("Saturday");
+
+		// write your code above this
+		System.out.println(namesList);
+		// get the element at index 3 and print the same
+		System.out.println("The element at the given index is " + namesList.get (3));
+	}
+}
