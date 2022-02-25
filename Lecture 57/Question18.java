c2c0ce730d107fdede1008e9f9bbbad6947e07ef
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-02-25 19:34:00 +0530

    Update Question18.java

--- Lecture 57/Question18.java
+++ Lecture 57/Question18.java
@@ -2,4 +2,11 @@ package q23676;
 import java.util.*;
 public class ArrayListIterationDemo {
 	public static void main(String[] args) {
-	
+	List < String > nameList = new ArrayList < String > ();
+	for (int i = 0; i<args.length;i++)
+	{
+		nameList.add (args[i]);
+	}
+	System.out.println ("Name at index 2 is : " + nameList.get (2));
+	}
+}
