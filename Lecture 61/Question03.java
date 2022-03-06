ebc3600435c97d78871274dc30813f9e8aff7d73
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-06 23:09:42 +0530

    Update Question03.java

--- Lecture 61/Question03.java
+++ Lecture 61/Question03.java
@@ -19,9 +19,6 @@ C<int> c4 = new C<int>(); // statement 7
 C<int[]> c3 = new C<int[]>(); // statement 8
 
 
-Statement 3 will result in a compilation error. Since class B is already using a type parameter T, class C cannot use a type parameter with same name.
-Statement 4 will result in a compilation error because only String or an Integer can be used as type argument and not a custom class such as A.
-Statement 5 will result in compilation error. Since class C is a generic class that accepts only one type parameter and not a nested type parameter.
-Statement 6 will not result in compilation errors.
-Statement 7 will not result in compilation error.
-Statement 8 will result in compilation error.
+          ------> A N S W E R <-----
+            
+D.) Statement 6 will not result in compilation errors.
