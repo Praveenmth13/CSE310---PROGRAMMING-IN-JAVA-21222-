5012ef554934536380cd93725ec1ea9cecadc8f0
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-17 22:41:08 +0530

    Update Question05.java

--- Lecture 46/Question05.java
+++ Lecture 46/Question05.java
@@ -1,1 +1,11 @@
-
+package q11398;
+import java.util.*;
+import java.text.*;
+public class DateFormatInJapan {
+	public static String getJapaneseShortDate(Date date) {
+		Format f=new SimpleDateFormat("yyyy/MM/dd");
+		String ans=f.format(date);
+		return ans;
+		
+	}
+}
