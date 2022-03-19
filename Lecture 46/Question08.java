2c5fcc416e0558f8b735f07a424907892963fb63
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-19 17:26:30 +0530

    Update Question08.java

--- Lecture 46/Question08.java
+++ Lecture 46/Question08.java
@@ -3,22 +3,21 @@ import java.util.*;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 public class CalendarAddition {
-	public String addDays(String dateString, int days) throws ParseException {
+		public String addDays(String dateString, int days) throws ParseException {
 		// use the correct format string as argument for the constructor
 		SimpleDateFormat sd = new SimpleDateFormat("MMM-dd-yyyy");
-		
-		// parses text from the beginning of the given string to produce a date
+									
+		// parses text from the beginning of the given string to produce a dateString		
 		Date date =  sd.parse(dateString);
-
 		Calendar cal = Calendar.getInstance();
-		
-		// configuring the cal object to the value of date
+															
+		// configuring the cal object to the value of dateString		
 		cal.setTime(date);
-
+																	
 		// supply the correct arguments to the add method below
 		cal.add(Calendar.DATE,days);
-
-		// format method on SimpleDateFormat returns a string representation of the passed date
+																					
+		// format method on SimpleDateFormat returns a string representation of the passed DATE		
 		return sd.format(cal.getTime());
-	}
+		}
 }
