c47e5aa854d7087d8ae8135e40c6e4f7482c59a9
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-19 21:05:36 +0530

    Update Question04.java

--- Lecture 41/Question04.java
+++ Lecture 41/Question04.java
@@ -1,4 +1,2 @@
-Statements 1 and 2 will result in compilation errors.
-Statement 3 will not result in a compilation error, since anonymous classes can have custom methods.
-Statement 4 will result in a compilation error as it is taking an argument "Hello", while there should have been only empty parenthesis.
-Statement 5 will result in compilation error, because it cannot declare extra methods other than those declared in the class Prefixer.
+A.) Statements 1 and 2 will result in compilation errors.  //ANSWER
+
