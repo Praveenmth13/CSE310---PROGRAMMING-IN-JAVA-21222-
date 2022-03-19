c30f5ba55340acb6ae7e7e60107733043e27fad5
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-19 16:09:29 +0530

    Update Question07.java

--- Lecture 46/Question07.java
+++ Lecture 46/Question07.java
@@ -6,17 +6,13 @@ public class CalendarDemo {
 	public String setTime(String dateString) throws ParseException {
 		// use the correct format string as argument for the constructor
 		SimpleDateFormat sd = new SimpleDateFormat("MMM-dd-yyyy");
-		
-		// parses text from the beginning of the given string to produce a date
-		Date date =  sd.parse(dateString);
-		
-		
-		Calendar currentTime = Calendar.getInstance();
-		
-		//// configuring the current object to the value of date
-    	currentTime.setTime(date);
-    	
-    	// format method on SimpleDateFormat returns a string representation of the passed date
-    	return sd.format(currentTime.getTime());
+			
+			// parses text from the beginning of the given string to produce a dateString
+			Date date =  sd.parse(dateString);
+				
+					
+			Calendar currentTime = Calendar.getInstance();
+			currentTime.setTime(date);
+			return sd.format(currentTime.getTime());
 	}
 }
