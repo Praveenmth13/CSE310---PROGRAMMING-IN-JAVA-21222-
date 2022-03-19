f2deb6d57b73a470c076d6c0f60a9c1d6ef5b36c
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-19 17:44:15 +0530

    Update Question08.java

--- Lecture 44/Question08.java
+++ Lecture 44/Question08.java
@@ -1,14 +1,12 @@
 package q24212;
-public class Example {
-	public static void main(String[] Args){
-		String s =  args[0];
-		StringBuilder sb = new StringBuilder();
-		for ( int i = 0; i < s.length(); i++){
-			char c = s.charAt(i);
-			if (Character.isUpperCase(c)){
-				sb.append(c);
-			}
+public class Example{
+		public static void main(String args[]){
+			String inputString=args[0];
+			String outputString = "";
+			for (int i = 0; i < inputString.length(); i++) {
+				char c = inputString.charAt(i);
+				outputString += Character.isUpperCase(c) ? c + "" : "";
+				}
+				System.out.println("The result is: "+ outputString);
 		}
-		system.out.println("The result is: " + sb.toString())
-	}
-} 
+}
