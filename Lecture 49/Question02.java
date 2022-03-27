d356a82b09fcbe0243c33d6a217a37362b31794e
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-27 10:07:36 +0530

    Update Question02.java

--- Lecture 49/Question02.java
+++ Lecture 49/Question02.java
@@ -1,27 +1,23 @@
 package q11335;
 public class ThrowExample {
 	public static void main(String args[]) {
-		
-		
 		int  age=Integer.parseInt(args[0]);
-		
 		int  wt=Integer.parseInt(args[1]);
-		
-		
 		System.out.println("Welcome to the Registration process!!");
 		try {
-			checkEligibilty(age,wt); // Fill the missing code
+			checkEligibilty(age,wt);
 			System.out.println("Have a nice day");
-		}
-		catch(Exception e) { // Fill the missing code
-			System.out.println("java.lang.ArithmeticException: Student is not eligible for registration"); // Fill the missing code
-		}
+			}
+			catch(Exception e) {
+				System.out.println("java.lang.ArithmeticException: Student is not eligible for registration"); 
+			}
 	}
-	static void checkEligibilty(int age, int wt) {
-		if(age<12 && wt<40) { // Write the condition
-			throw new ArithmeticException("Student is not eligible for registration"); // Fill the missing code
-		} else {
-			System.out.println("Student Entry is Valid!!");
-		}
-	}
+			static void checkEligibilty(int age, int wt) {
+				if(age<12 && wt<40) {
+				throw new ArithmeticException("Student is not eligible for registration"); 
+				} else {
+					System.out.println("Student Entry is Valid!!");
+					
+				}
+			}
 }
