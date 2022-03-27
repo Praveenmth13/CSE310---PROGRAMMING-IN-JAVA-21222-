cfcf86cf8769cb60b086f198bd148c760e6647d2
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-27 09:12:50 +0530

    Update Question06.java

--- Lecture 47/Question06.java
+++ Lecture 47/Question06.java
@@ -2,12 +2,12 @@ package q11322;
 public class TryCatchDemo {
 	public static void main(String[] args) {
 		System.out.println("Before sleep...");
-	try{	Thread.sleep(2000);
-	}catch(InterruptedException e) {	
-		e.printStackTrace();
-		
-	}
+		try{
+		Thread.sleep(2000);
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+		}
 		System.out.println("After sleep...");
-	
-}
+	}
 }
