a32744075696c30896460c725068d07c4c89ecb5
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-27 09:35:49 +0530

    Update Question05.java

--- Lecture 48/Question05.java
+++ Lecture 48/Question05.java
@@ -1,26 +1,14 @@
 package q11329;
 public class Division {
-	
-	public static void main (String args[]) {
-		
-		int num1 = Integer.parseInt(args[0]);
-		
-		int num2 = Integer.parseInt(args[1]),result = 0;
-		
-		try{
-			
-			result = num1/num2;
-			
-			System.out.println("Result = " +result);
-			
-		}
-		
-		catch (ArithmeticException e) {
-			
-			System.out.println ("Exception caught : divide by zero occurred" );
-			
-		}
-		
-	}
-	
+	public static void main (String args[]) {		
+		int num1 = Integer.parseInt(args[0]);		
+		int num2 = Integer.parseInt(args[1]),result = 0;		
+		try{			
+			result = num1/num2;			
+			System.out.println("Result = " +result);			
+		}		
+		catch (ArithmeticException e) {		
+			System.out.println ("Exception caught : divide by zero occurred" );			
+		}		
+	}	
 }
