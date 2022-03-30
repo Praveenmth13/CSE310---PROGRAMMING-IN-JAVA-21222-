366d7ceab125295ff818e99bc4796bc4f5a662f7
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-31 01:28:56 +0530

    Update Question01.java

--- Lecture 65/Question01.java
+++ Lecture 65/Question01.java
@@ -1,1 +1,47 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+interface Cube {
+    int Compute(int X);
 
+}
+class UserdefinedFunctionalInterface {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int A = sc.nextInt();
+        Cube C = (int X) -> X * X * X;
+        System.out.println("Cube of value is " + C.Compute(A));
+    }
+}
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class FunctionalInterface {
+    public static void main(String args[]) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Creation of new thread");
+            }
+        }).start();
+    }
+}
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class FunctionalInterfaceWithLambda {
+    public static void main(String args[]) {
+        new Thread(() -> {
+            System.out.println("Creation of new thread");
+        }).start();
+    }
+}
