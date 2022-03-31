75f7102ec13ac18bb862369c6b2ba1c2fa4b2631
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-03-31 19:56:22 +0530

    Update Question01 to 08.java

--- Lecture 65/Question01 to 08.java
+++ Lecture 65/Question01 to 08.java
@@ -112,3 +112,23 @@ class LambdaExpression{
 
 
 
+// Complete below given code as per the problem statement
+import java.util.ArrayList;
+class LambdaExpression{
+	public static void main(String args[]){
+		ArrayList<Integer> ArrList = new ArrayList<Integer>();
+		ArrList.add(120);
+		ArrList.add(303);
+		ArrList.add(308);
+		ArrList.add(555);
+		//Complete the code for displaying elements using lambda expression
+		ArrList.forEach(n -> System.out.println(n));
+		System.out.println("All odd elements displayed");
+		  ArrList.forEach(n->{if (n%2 !=0) System.out.println(n);});
+		
+	}
+}
+
+
+
+
